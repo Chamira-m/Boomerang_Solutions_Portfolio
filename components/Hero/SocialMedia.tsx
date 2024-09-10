bf039e7f6ad71df6e148e91d8aeaec1aa42158bd
Chamira Gunawardhana <chamiramadusankha96@gmail.com>
2024-09-10 11:59:00 +0530

    change blog contact

--- components/Hero/SocialMedia.tsx
+++ components/Hero/SocialMedia.tsx
@@ -6,22 +6,22 @@ const SocialMedia = () => {
     <div className=" 3xl:mt-6 mt-1 flex flex-col p-2 md:mt-2 md:p-3 2xl:mt-5 ">
       <div className="flex w-full">
         <SocialMediaButton
-          link="https://www.facebook.com/Torenshal/"
+          link="https://www.facebook.com/"
           style="4xl:w-[45px] 4xl:h-[45px] 2xl:w-[24px] 2xl:h-[24px] md:w-[18px] md:h-[18px] w-[17px] h-[17px] mr-2 cursor-pointer"
           image="/images/socialmedia/facebook.png"
         />
         <SocialMediaButton
-          link="https://x.com/torenshal?s=21&t=60aAmVYPlQbYmk6HAEPD1Q"
+          link="https://x.com/"
           style="4xl:w-[45px] 4xl:h-[45px] 2xl:w-[24px] 2xl:h-[24px] md:w-[18px] md:h-[18px] w-[17px] h-[17px] mr-2 cursor-pointer"
           image="/images/socialmedia/twitter.png"
         />
         <SocialMediaButton
-          link="https://www.linkedin.com/company/torenshal"
+          link="https://www.linkedin.com/"
           style="4xl:w-[45px] 4xl:h-[45px] 2xl:w-[24px] 2xl:h-[24px] md:w-[18px] md:h-[18px] w-[17px] h-[17px] mr-2 cursor-pointer"
           image="/images/socialmedia/linkedin.png"
         />
         <SocialMediaButton
-          link="https://www.instagram.com/torenshal.digital/"
+          link="https://www.instagram.com/"
           style="4xl:w-[45px] 4xl:h-[45px] 2xl:w-[24px] 2xl:h-[24px] md:w-[18px] md:h-[18px] w-[17px] h-[17px] mr-2 cursor-pointer"
           image="/images/socialmedia/instagram.png"
         />
